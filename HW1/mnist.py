7dd8e3fa68808e415f7c9b748d8ba30f61333dc8
thomasw219 <thomasw219@gmail.com>
2020-01-31 17:10:31 -0600

    finished covariance matrix and eigenvalue, eigenvector stuff

--- HW1/mnist.py
+++ HW1/mnist.py
@@ -25,10 +25,32 @@ def load_data(s, num_images):
 
 def sample_covariance(images):
     cov_mat = np.zeros((num_pixels, num_pixels))
+    mean = np.mean(images, axis=0)
     for image in images:
-        cov_mat += image * image.T
+        cv = np.outer(image - mean, image - mean)
+        cov_mat += cv
+        """
+        image = np.asarray(cv.reshape(num_pixels, num_pixels, 1).astype(np.float32)).squeeze()
+        plt.imshow(image)
+        plt.show()
+        """
+    cov_mat *= 1 / images.shape[0]
+    return cov_mat
 
 train_images, train_labels = load_data('train', 60000)
 test_images, test_labels = load_data('t10k', 10000)
 
-print(sample_covariance(train_images))
+"""
+cov_mat = sample_covariance(train_images)
+np.save("cov_mat", cov_mat)
+"""
+cov_mat = np.load("cov_mat.npy")
+print(cov_mat.shape)
+w, v = np.linalg.eig(cov_mat)
+print(w)
+print((np.matmul(cov_mat, v[0]).shape))
+for i in range(10):
+    image = np.asarray((np.matmul(cov_mat, v[i])).reshape(image_size, image_size, 1).astype(np.float32)).squeeze()
+#image = np.asarray(cov_mat).squeeze()
+    plt.imshow(image)
+    plt.show()
