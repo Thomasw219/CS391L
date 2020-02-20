cc285486cbb9725c3a0bf3d73c535487346e1f9b
thomasw219 <thomasw219@gmail.com>
2020-02-19 20:11:33 -0600

    some normalization stuff mostly

--- HW2/ICA.py
+++ HW2/ICA.py
@@ -26,14 +26,18 @@ def gradient_ascent(W_i, grad, eta, n, X):
     return W_i + eta * ave_grad
 
 def normalize(s_i):
-    s_i += np.min(s_i)
-    s_i /= np.max(s_i) / 2
+    m = np.min(s_i)
+    if m >= 0:
+        s_i -= np.min(s_i)
+    else:
+        s_i += np.min(s_i)
+    s_i /= np.max(s_i) / 1
     return s_i
 
 def l2_loss(s_i, s_f):
     total = 0
-    for i in s_i.shape[0]:
-        total += np.abs(s_i[i] - s_j[i])
+    for i in range(s_i.shape[0]):
+        total += (s_i[i] - s_f[i])**2
     return total
 
 def total_loss(S_i, S_f):
@@ -42,7 +46,7 @@ def total_loss(S_i, S_f):
     for i in range(m):
         losses = []
         for j in range(m):
-            losses.append(l2_loss(S_i[i], S_j[j]))
+            losses.append(l2_loss(normalize(S_i[i]), normalize(S_f[j])))
         total += min(losses)
     return total
 
@@ -55,18 +59,42 @@ A = test['A']
 U = test['U']
 X = np.matmul(A, U)
 W = np.random.rand(A.shape[0], A.shape[1])
+idx = np.arange(1, U.shape[1] + 1)
 n = 20
-iterations = 20000
-eta = 0.025
+iterations = 1000000
+eta = 0.01
 
+least_loss = np.inf
 for i in range(iterations):
     indices = np.random.choice(U.shape[1], size=n)
     batch = np.transpose(U[:,indices])
     W = gradient_ascent(W, grad, eta, n, batch)
+    S = np.matmul(W, X)
+    l = total_loss(U, S)
 
-S = np.matmul(W, X)
-idx = np.arange(1, U.shape[1] + 1)
-plt.figure(0)
+    if l < least_loss:
+        least_loss = l
+        print(l)
+
+        """
+        plt.figure(i)
+        plt.subplot(6, 1, 1)
+        plt.plot(idx, U[0])
+        plt.subplot(6, 1, 2)
+        plt.plot(idx, U[1])
+        plt.subplot(6, 1, 3)
+        plt.plot(idx, U[2])
+        plt.subplot(6, 1, 4)
+        plt.plot(idx, normalize(S[0]))
+        plt.subplot(6, 1, 5)
+        plt.plot(idx, normalize(S[1]))
+        plt.subplot(6, 1, 6)
+        plt.plot(idx, normalize(S[2]))
+        plt.savefig("./figures/" + str(i) + ".png")
+        plt.close()
+        """
+
+plt.figure(iterations)
 plt.subplot(6, 1, 1)
 plt.plot(idx, U[0])
 plt.subplot(6, 1, 2)
@@ -79,4 +107,5 @@ plt.subplot(6, 1, 5)
 plt.plot(idx, normalize(S[1]))
 plt.subplot(6, 1, 6)
 plt.plot(idx, normalize(S[2]))
-plt.show()
+plt.savefig("./figures/" + str(iterations) + ".png")
+plt.close()
