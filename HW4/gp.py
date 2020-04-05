9628467a0f96ad5fc20a268089b73c5dfbbcaab1
thomasw219 <thomasw219@gmail.com>
2020-04-05 02:19:17 -0500

    refactor and add logprob function

--- HW4/gp.py
+++ HW4/gp.py
@@ -1,10 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def SE_kernel(x_1, x_2, sigma_f=10, sigma_l=1):
-    arg = -1 * np.linalg.norm(x_1 - x_2)**2 / (2 * sigma_l**2)
-    return sigma_f * np.exp(arg)
-
 def delta(x_1, x_2):
     if np.array_equal(x_1, x_2):
         return 1
@@ -26,36 +22,57 @@ def conditional_variance(K_ff, K_fy, K_yy_inv):
     temp = np.matmul(K_yy_inv, np.transpose(K_fy))
     return K_ff - np.matmul(K_fy, temp)
 
+def Cov(X_1, X_2, ker, sig_f, sig_l, sig_n):
+    K = np.zeros((X_1.shape[0], X_2.shape[0]))
+    sym = np.array_equal(X_1, X_2)
+    for i in range(K.shape[0]):
+        for j in range(K.shape[1]):
+            if not sym:
+                K[i, j] = ker(X_1[i], X_2[j], sig_f, sig_l, sig_n)
+            else:
+                if K[j, i] != 0:
+                    K[i, j] = K[j, i]
+                else:
+                    K[i, j] = ker(X_1[i], X_2[j], sig_f, sig_l, sig_n)
+    return K
+
 def define_GP(data_X, data_Y, m_y, m_f, sample_X, sig_f, sig_l, sig_n):
-    K_yy = np.zeros((data_X.shape[0], data_X.shape[0]))
-    for i in range(data_X.shape[0]):
-        for j in range(data_X.shape[0]):
-            K_yy[i, j] = k(data_X[i], data_X[j], sig_f, sig_l, sig_n)
+    K_yy = Cov(data_X, data_X, k, sig_f, sig_l, sig_n)
     K_yy_inv = np.linalg.inv(K_yy)
-
-    K_fy = np.zeros((sample_X.shape[0], data_X.shape[0]))
-    for i in range(sample_X.shape[0]):
-        for j in range(data_X.shape[0]):
-            K_fy[i, j] = kprime(sample_X[i], data_X[j], sig_f, sig_l, sig_n)
-
-    K_ff = np.zeros((sample_X.shape[0], sample_X.shape[0]))
-    for i in range(sample_X.shape[0]):
-        for j in range(sample_X.shape[0]):
-            K_ff[i, j] = kprime(sample_X[i], sample_X[j], sig_f, sig_l, sig_n)
-
+    K_fy = Cov(sample_X, data_X, kprime, sig_f, sig_l, sig_n)
+    K_ff = Cov(sample_X, sample_X, kprime, sig_f, sig_l, sig_n)
     sample_mean = conditional_mean(K_yy_inv, K_fy, data_Y, m_y, m_f)
     sample_cov_mat = conditional_variance(K_ff, K_fy, K_yy_inv)
     return sample_mean, sample_cov_mat
 
-#def dkdsigf(x_1, x_2, sig_f, sig_l, sig_n):
+def dkdsig_f(x_1, x_2, sig_f, sig_l, sig_n):
+    return kprime(x_1, x_2, sig_f, sig_l, sig_n)
 
+def dkdsig_l(x_1, x_2, sig_f, sig_l, sig_n):
+    arg = -1 / 2 * np.exp(sig_l) * np.linalg.norm(x_1 - x_2)**2
+    return kprime(x_1, x_2, sig_f, sig_l, sig_n) * arg
+
+def dkdsig_n(x_1, x_2, sig_f, sig_l, sig_n):
+    return np.exp(sig_n) * delta(x_1, x_2)
+
+data_X = np.reshape(np.array([204, 90, 400]), (3, 1))
+data_Y = np.array([-7, -18, 20])
+N = data_X.shape[0]
+def logprob(sig):
+    sig_f = sig[0]
+    sig_l = sig[1]
+    sig_n = sig[2]
+
+    Q = Cov(data_X, data_X, k, sig_f, sig_l, sig_n)
+    term1 = -1 / 2 * np.matmul(np.matmul(np.transpose(data_Y), np.linalg.inv(Q)), data_Y)
+    term2 = -1 / 2 * np.log(np.linalg.det(Q))
+    term3 = N / 2 * log(2 * np.pi)
+    return term1 + term2 + term3
 
 SIG_F = 2.3
-SIG_L = -8
+SIG_L = -7.8
 SIG_N = 0
 
-data_Y = np.array([-7, -18, 20])
-data_X = np.reshape(np.array([204, 90, 400]), (3, 1))
 m_y = np.array([0, 0, 0])
 sample_X = np.arange(0, 500, 1)
 sample_X = np.reshape(sample_X, (sample_X.shape[0], 1))
@@ -71,4 +88,3 @@ plt.plot(sample_X, sample_mean, color='r')
 plt.plot(sample_X, sample_mean + 2 * sample_var, color='b')
 plt.plot(sample_X, sample_mean - 2 * sample_var, color='b')
 plt.show()
-
