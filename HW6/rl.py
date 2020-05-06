67703834686cfa4c0a930c5a58a1a8511dc8826f
thomasw219 <thomasw219@gmail.com>
2020-05-06 01:13:19 -0500

    really nothing :(

--- HW6/rl.py
+++ HW6/rl.py
@@ -1,3 +1,12 @@
 import numpy as np
 
+from collections import namedtuple
+
+FullState = namedtuple('position', 'obstacles', 'litter')
+
+SidewalkState = namedtuple('position')
+ForwardState = namedtuple('position')
+ObstaclesState = namedtuple('position', 'obstacles')
+LitterState = namedtuple('position', 'litter')
+
 
